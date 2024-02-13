5404efa974576121685556dc20ac8125f50490d6
bkadel1 <144166993+bkadel1@users.noreply.github.com>
2024-02-12 21:13:15 -0500

    added random image function

--- script1.js
+++ script1.js
@@ -2,17 +2,16 @@ const breedInput = document.getElementById('breedInput');
 const autocomplete = document.getElementById('autocomplete');
 const showImagesBtn = document.getElementById('showImagesBtn');
 const imageContainer = document.getElementById('imageContainer');
-
 let breedsList = [];
+let intervalId;
 
-// Fetch dog breeds list from Dog CEO API
 fetch('https://dog.ceo/api/breeds/list/all')
   .then(response => response.json())
   .then(data => {
     breedsList = Object.keys(data.message);
   });
 
-// Autocomplete function
+
 breedInput.addEventListener('input', () => {
   const inputText = breedInput.value.toLowerCase();
   const matchedBreeds = breedsList.filter(breed => breed.toLowerCase().startsWith(inputText));
@@ -29,21 +28,32 @@ breedInput.addEventListener('input', () => {
   });
 });
 
-// Show Images button click event
+
+function fetchAndDisplayImages(selectedBreed) {
+  imageContainer.innerHTML = '';
+  fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/1`)
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data.message;
+      const img = document.createElement('img');
+      img.src = imageUrl;
+      imageContainer.appendChild(img);
+    })
+    .catch(error => {
+      console.error('Error fetching image:', error);
+      imageContainer.innerHTML = '<p>Error fetching image</p>';
+    });
+}
+
 showImagesBtn.addEventListener('click', () => {
   const selectedBreed = breedInput.value.toLowerCase();
+  clearInterval(intervalId);
   if (breedsList.includes(selectedBreed)) {
-    imageContainer.innerHTML = '';
-    fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/2`)
-      .then(response => response.json())
-      .then(data => {
-        data.message.forEach(imageUrl => {
-          const img = document.createElement('img');
-          img.src = imageUrl;
-          imageContainer.appendChild(img);
-        });
-      });
+    fetchAndDisplayImages(selectedBreed);
+    intervalId = setInterval(() => {
+      fetchAndDisplayImages(selectedBreed);
+    }, 5000);
   } else {
-    imageContainer.innerHTML = '<p>No such breed found</p>';
+    imageContainer.innerHTML = '<p>No such breed</p>';
   }
 });
