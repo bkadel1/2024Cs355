87f116798c1c6c451c7024b942fbb41837d1ddfa
bkadel1 <144166993+bkadel1@users.noreply.github.com>
2024-03-18 17:25:43 -0400

    added js file

--- express-server/index.js
+++ express-server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const Datastore = require('nedb-promises');
-const path = require('path');
 
 const app = express();
-const db = Datastore.create({ filename: path.join(__dirname, 'hits.jsonl'), autoload: true });
+const db = Datastore.create('hits.jsonl');
+
+app.use(express.static('public'))
 
 let hitCounter = 0;
 
-// Check if the database contains hits data
+
 db.findOne({ hits: { $exists: true } })
   .then(doc => {
     if (doc) {
@@ -16,7 +17,7 @@ db.findOne({ hits: { $exists: true } })
   })
   .catch(err => console.error('Error loading hit count:', err));
 
-// Middleware to increase hit counter and update database
+
 app.use((req, res, next) => {
   hitCounter++;
   db.update({ hits: { $exists: true } }, { hits: hitCounter }, { upsert: true })
@@ -24,16 +25,16 @@ app.use((req, res, next) => {
   next();
 });
 
-// Route to get the number of hits
+
 app.get('/hits', (req, res) => {
   res.send(String(hitCounter));
 });
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'static')));
 
-// Start the server
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
